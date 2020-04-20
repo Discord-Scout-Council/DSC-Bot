ed8fe3d5729805b89c5711ca22ca2d708dfab7e1
Owen Salter <owen@devosmium.xyz>
2020-04-20 17:55:34 -0400

    feat(help): Add command descriptions
    Added to ping and about

--- src/commands/general.rs
+++ src/commands/general.rs
@@ -1,6 +1,8 @@
 use serenity::framework::standard::{macros::command, CommandResult, StandardFramework};
 use serenity::{model::channel::Message, prelude::*};
+
 #[command]
+#[description = "Pings the bot"]
 pub fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
     if let Err(err) = msg.channel_id.say(&ctx.http, "Pong!") {
         println!("Err sending message: {}", err);
@@ -10,6 +12,7 @@ pub fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
 }
 
 #[command]
+#[description = "Provides helpful information about the bot"]
 pub fn about(ctx: &mut Context, msg: &Message) -> CommandResult {
     msg.channel_id.send_message(&ctx.http, |m | {
         m.embed(|e | {
