efbd2b471576c39a5c6fcd0bce8e77ffbcc6fe51
Owen Salter <owen@devosmium.xyz>
2020-04-22 17:04:12 -0400

    chore(fmt): Run cargo fmt

--- src/checks/mod.rs
+++ src/checks/mod.rs
@@ -1,21 +1,22 @@
-use serenity::{
-    framework::standard::{
-        CheckResult, Args, CommandOptions, macros::check
-    },
-    model::channel::{Message}
-};
 use serenity::prelude::*;
+use serenity::{
+    framework::standard::{macros::check, Args, CheckResult, CommandOptions},
+    model::channel::Message,
+};
 
 #[check]
 #[name = "Moderator"]
 #[display_in_help]
-pub fn mod_check(ctx: &mut Context, msg: &Message, _: &mut Args, _: &CommandOptions) -> CheckResult {
+pub fn mod_check(
+    ctx: &mut Context,
+    msg: &Message,
+    _: &mut Args,
+    _: &CommandOptions,
+) -> CheckResult {
     if let Some(member) = msg.member(&ctx.cache) {
-
         if let Ok(permissions) = member.permissions(&ctx.cache) {
             return permissions.manage_guild().into();
         }
-
     }
 
     false.into()
