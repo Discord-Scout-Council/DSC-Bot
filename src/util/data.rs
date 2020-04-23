154ee8277fc5ab494f450518abaf3855853322fd
Owen Salter <owen@devosmium.xyz>
2020-04-23 16:45:13 -0400

    feat(util): Add helper method to get global databases

--- src/util/data.rs
+++ src/util/data.rs
@@ -19,6 +19,17 @@ pub fn get_pickle_database(guild_id: &u64, db_name: &str) -> PickleDb {
 
 }
 
+pub fn get_global_pickle_database(db_name: &str) -> PickleDb {
+    let mut path: String = String::from("./data/");
+    path.push_str(db_name);
+    let db = match PickleDb::load_yaml(&path, PickleDbDumpPolicy::AutoDump) {
+        Ok(d) => d,
+        Error => PickleDb::new_yaml(&path, PickleDbDumpPolicy::AutoDump),
+    };
+
+    db
+}
+
 fn construct_data_path(guild_id: &u64, db_name: &str) -> String {
     let mut path = String::from("./data/");
     path.push_str(&guild_id.to_string());
