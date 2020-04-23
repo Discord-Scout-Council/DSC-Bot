7a7fdfd4237a3679ddb749648dce3a21d404255c
Owen Salter <owen@devosmium.xyz>
2020-04-23 13:50:19 -0400

    feat(qotd): Add QOTD run command

--- src/commands/qotd.rs
+++ src/commands/qotd.rs
@@ -36,7 +36,7 @@ impl PartialEq for Question {
 
 #[command]
 #[description = "Manages the Question of the Day"]
-#[sub_commands(add)]
+#[sub_commands(add, run)]
 pub fn qotd(ctx: &mut Context, msg: &Message) -> CommandResult {
     msg.channel_id.say(&ctx.http, "Base command");
 
@@ -53,7 +53,6 @@ pub fn add(ctx: &mut Context, msg: &Message, args: Args) -> CommandResult {
     message.push_str(&" to the question of the day");
     println!("Opening QOTDatabase");
     let mut db = PickleDb::load_yaml("qotd.db", PickleDbDumpPolicy::AutoDump)?;
-    //let guild_cache = PickleDb::load_yaml("guild_cache.db", PickleDbDumpPolicy::AutoDump)?;
     println!("Getting all keys");
     let mut db_keys = db.get_all();
 
@@ -76,5 +75,43 @@ pub fn add(ctx: &mut Context, msg: &Message, args: Args) -> CommandResult {
     println!("Adding question");
     db.set(&current_num.to_string(), &args.rest());
     msg.channel_id.say(&ctx.http, message).unwrap();
+    Ok(())
+}
+
+#[command]
+#[description = "Runs the Question of the Day based on the last run question"]
+#[checks(Moderator)]
+pub fn run(ctx: &mut Context, msg: &Message) -> CommandResult {
+    let db = PickleDb::load_yaml("qotd.db", PickleDbDumpPolicy::AutoDump)?;
+    let mut guild_cache = PickleDb::load_yaml("guild_cache.db", PickleDbDumpPolicy::AutoDump)?;
+    let current_num: i32 = guild_cache.get::<i32>("current_qotd").unwrap() + 1;
+    let next_question = Question {num: current_num + 1, text: db.get(&current_num.to_string()).unwrap()};
+
+    println!("Finding qotd channel");
+    let guild_arc = msg.guild(&ctx).unwrap();
+    let guild = guild_arc.read();
+    let qotd_channel = guild.channel_id_from_name(&ctx, "qotd-questions").unwrap();
+
+    qotd_channel.send_message(&ctx.http, |m| {
+        m.embed(|e| {
+            e.title("Question of the Day");
+            e.description(&next_question.text);
+            e.footer(|f| {
+                let mut text: String = String::from("Requested by ");
+                text.push_str(&msg.author.name);
+                f.text(text);
+
+                f
+            });
+
+            e
+        });
+
+        m
+    })?;
+    println!("Sent message");
+
+    guild_cache.set("current_qotd", &current_num)?;
+
     Ok(())
 }
