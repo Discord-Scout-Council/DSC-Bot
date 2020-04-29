7c70e2bec47b64f808632db2006f3312dab82f9b
Owen Salter <owen@devosmium.xyz>
2020-04-29 13:50:25 -0400

    test(leveling): Update tests to use new equation

--- src/util/leveling.rs
+++ src/util/leveling.rs
@@ -21,12 +21,12 @@ mod tests {
     use super::*;
     #[test]
     fn get_level_cost() {
-        let actual_cost = 250u64;
+        let actual_cost = 17u64;
         assert_eq!(actual_cost, super::get_level_cost(2));
     }
     #[test]
     fn level_from_points() {
         let actual_level = 2u32;
-        assert_eq!(actual_level, super::get_level_from_points(251u64));
+        assert_eq!(actual_level, super::get_level_from_points(18u64));
     }
 }
