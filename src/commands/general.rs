8bbf880bb4443e9c776906ec1b5e1a93f00a564c
Owen Salter <owen@devosmium.xyz>
2020-04-29 14:41:43 -0400

    fix(general): Update about command to new project hub

--- src/commands/general.rs
+++ src/commands/general.rs
@@ -24,8 +24,7 @@ pub fn about(ctx: &mut Context, msg: &Message) -> CommandResult {
             e.description("A Discord Bot for Camp Quarantine");
             e.field("Creator", "<@118455061222260736>", true);
             e.field("Logo", "<@678816040146436117>", true);
-            e.field("Git", "[git.sr.ht](https://git.sr.ht/~muirrum/campmasteronstantine)", false);
-            e.field("Issues", "[todo.sr.ht](https://todo.sr.ht/~muirrum/Campmaster-Constantine)", true);
+            e.field("Project Hub", "[hub.sr.ht](https://hub.sr.ht/~muirrum/Campmaster-Constantine/)", false);
             e.field("Report an Issue or Suggestion", "cbotsuggest <Suggestion>", true);
 
             e.thumbnail("https://cdn.discordapp.com/attachments/697917247368462336/702621900022480986/image0.png");
