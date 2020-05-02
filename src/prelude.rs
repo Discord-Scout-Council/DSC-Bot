968091a91ff01699773159b2c8af704074d0a0e8
Owen Salter <owen@devosmium.xyz>
2020-05-02 07:15:06 -0400

    refactor(prelude): Add log macros to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -8,3 +8,4 @@ pub use serenity::framework::standard::{macros::command, Args, CommandResult};
 pub use serenity::model::id::UserId;
 pub use serenity::utils::Colour;
 pub use serenity::{model::channel::Message, model::guild::Member, prelude::*};
+pub use log::{debug,info,error,warn};
