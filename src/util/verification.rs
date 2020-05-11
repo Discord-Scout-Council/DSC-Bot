88363d71a6b43596946b918b97710e6e247d71fc
Owen Salter <owen@devosmium.xyz>
2020-05-11 10:28:22 -0400

    Convert utility functions to async

--- src/util/verification.rs
+++ src/util/verification.rs
@@ -18,14 +18,14 @@ enum VerifyType {
     Close,
 }
 
-pub fn handle_verification_file(ctx: &Context, msg: &Message) -> Result<(), String> {
+async fn handle_verification_file(ctx: &Context, msg: &Message) -> Result<(), String> {
     if msg.attachments.len() == 0 {
         return Err(String::from("No attachments found"));
     }
 
     let http_cache = &ctx.http;
 
-    let verify_channel = match http_cache.get_channel(684577265425973285) {
+    let verify_channel = match http_cache.get_channel(684577265425973285).await {
         Ok(c) => c,
         Err(err) => {
             return Err(err.to_string());
@@ -41,7 +41,7 @@ pub fn handle_verification_file(ctx: &Context, msg: &Message) -> Result<(), Stri
             msg.attachments.get(0).unwrap().url
         ));
         m
-    }) {
+    }).await {
         Err(err) => return Err(err.to_string()),
         Ok(m) => m,
     };
@@ -49,7 +49,7 @@ pub fn handle_verification_file(ctx: &Context, msg: &Message) -> Result<(), Stri
     let emoji_vec = define_emoji_vec();
 
     for s in emoji_vec {
-        if let Err(err) = verify_message.react(http_cache, s) {
+        if let Err(err) = verify_message.react(http_cache, s.chars().next().unwrap()).await {
             return Err(err.to_string());
         }
     }
@@ -66,7 +66,7 @@ pub fn handle_verification_file(ctx: &Context, msg: &Message) -> Result<(), Stri
             e
         });
         m
-    }) {
+    }).await {
         return Err(format!(
             "Error sending successful submission message: {:?}",
             err.to_string()
@@ -76,14 +76,14 @@ pub fn handle_verification_file(ctx: &Context, msg: &Message) -> Result<(), Stri
     Ok(())
 }
 
-pub fn handle_verification_reaction(ctx: &Context, react: Reaction) -> Result<String, String> {
-    let current_info = &ctx.http.get_current_application_info().unwrap();
+async fn handle_verification_reaction(ctx: &Context, react: Reaction) -> Result<String, String> {
+    let current_info = &ctx.http.get_current_application_info().await.unwrap();
     if react.user_id.as_u64() == current_info.id.as_u64() {
         return Ok(String::from(""));
     }
     let message_id = react.message_id;
     let http_cache = &ctx.http;
-    let message = match http_cache.get_message(684577265425973285, *message_id.as_u64()) {
+    let message = match http_cache.get_message(684577265425973285, *message_id.as_u64()).await {
         Ok(m) => m,
         Err(err) => return Err(err.to_string()),
     };
@@ -107,12 +107,12 @@ pub fn handle_verification_reaction(ctx: &Context, react: Reaction) -> Result<St
     let split_contents = user_id.collect::<Vec<&str>>();
     let user_id_str: &str = split_contents.get(0).unwrap();
 
-    let user = match http_cache.get_user(split_contents.get(0).unwrap().parse::<u64>().unwrap()) {
+    let user = match http_cache.get_user(split_contents.get(0).unwrap().parse::<u64>().unwrap()).await {
         Err(err) => return Err(err.to_string()),
         Ok(u) => u,
     };
 
-    let priv_chan = match user.create_dm_channel(http_cache) {
+    let priv_chan = match user.create_dm_channel(http_cache).await {
         Ok(c) => c,
         Err(err) => return Err(err.to_string()),
     };
@@ -139,14 +139,14 @@ pub fn handle_verification_reaction(ctx: &Context, react: Reaction) -> Result<St
                     e
                 });
                 m
-            }) {
+            }).await {
                 return Err(format!(
                     "Could not send closed request message to {}: {:?}",
                     user_id_str.to_string(),
                     err
                 ));
             }
-            if let Err(err) = message.delete(http_cache) {
+            if let Err(err) = message.delete(http_cache).await {
                 return Err(format!(
                     "Could not delete verification message: {:?}",
                     err.to_string()
@@ -172,7 +172,7 @@ pub fn handle_verification_reaction(ctx: &Context, react: Reaction) -> Result<St
             e
         });
         m
-    }) {
+    }).await {
         return Err(format!(
             "Error sending verification success message {:?}",
             err.to_string()
